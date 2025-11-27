177583ce993c65f62936bb14edb8b4a0eab7e86d
adamgdaniel <adamgdaniel@gmail.com>
2025-11-27 17:31:15 +0100

    Kafka_mensajes_reales
    Kafka funcionando con mensajes reales en pull_api.py y enviandolos a un consumer.
    Falta crear los streams y crear un consumer para cada zona y el del CECOPI.

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -32,14 +32,15 @@ try:
             datos = json.loads(texto)
             
             # Extraemos los datos que te interesan
-            nombre = datos.get("nombre")
-            estado = datos.get("estado")
+            estacion = datos.get("estacion")
+            estado = datos.get("alerta_activa")
+            valor = datos.get("nivel_no2")
 
-            if estado == "alerta":
-                print(f"!!! ALERTA DETECTADA en: {nombre} !!!")
+            if estado == True:
+                print(f"!!! ALERTA DETECTADA en: {estacion} !!!")
                 print(f"Datos completos: {datos}")
             else:
-                print(f"Mensaje normal de {nombre}: {estado}")
+                print(f"Mensaje normal de {estacion}: Niveles de NO2: {valor} µg/m³")
 
         # Si el mensaje NO es JSON (ej. "Producer iniciado"), entra aquí
         except json.JSONDecodeError:
