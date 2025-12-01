78cd86a14f4dcc764bf54db2ea8bf0cda4f6ae43
adamgdaniel <adamgdaniel@gmail.com>
2025-12-01 13:40:38 +0100

    Update consumer_generator.py

--- consumer_generator.py
+++ consumer_generator.py
@@ -1,5 +1,6 @@
 from confluent_kafka import Producer, Consumer
 import json
+import time
 #from producer import fetch_from_db
 
 # sensores = fetch_from_db()
@@ -38,52 +39,23 @@ consumer.subscribe(['alertas_zona_{estaciones[int(selection)]}'])
 print(f"Conectado a alertas para la zona de: {estaciones[int(selection)]}, esperando mensajes...\n")
 
 
-
 try:
     while True:
-        # Buscamos mensajes cada 1 segundo
-        msg = consumer.poll(5.0)
-
+        msg = consumer.poll(1.0)
         if msg is None:
-            print(".", end="", flush=True)
             continue
         if msg.error():
             print(f"Error: {msg.error()}")
             continue
 
-        print(f"\n📩 Mensaje recibido!")
-        # Decodificamos el mensaje que llega en bytes
-        texto = msg.value().decode('utf-8')
+        datos = json.loads(msg.value().decode('utf-8'))
+        momento = time.strftime("%Y-%m-%d %H:%M:%S")
 
-        # Intentamos leerlo como JSON (para tu sistema de alertas)
-        try:
-            datos = json.loads(texto)
-            
-            # Extraemos los datos
-            estacion = datos.get("estacion")
-            estado = datos.get("alerta_activa")
-            valor = datos.get("nivel_no2")
-            fecha_carg = datos.get("fecha_carg", "")
+        print(f"\n📩 Alerta recibida para la zona de {estaciones[int(selection)]}!")           
 
-            if estado == True:
-                print(f"!!! ALERTA DETECTADA en: {estacion} !!!")
-                print(f"Datos completos: {datos}")
-                # Agregar al buffer y guardar
-                alertas_buffer.append({
-                    "nombre": estacion,
-                    "nivel_no2": valor,
-                    "fecha_carg": fecha_carg
-                })
-                save_alertas(alertas_buffer)
-            else:
-                print(f"Mensaje normal de {estacion}: Niveles de NO2: {valor} µg/m³")
-
-        # Si el mensaje NO es JSON (ej. "Producer iniciado"), entra aquí
-        except json.JSONDecodeError:
-            print(f"Mensaje no-JSON: {texto}")
 
 except KeyboardInterrupt:
-    print(f"Mensaje no-JSON: {texto}")
-    save_alertas(alertas_buffer)
+    print("Monitoreo de la zona {estaciones[int(selection)]} detenido.")
+finally:
     consumer.close()
 
