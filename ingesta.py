af5b79c719e2a419a5f5839de59d05edc2b89d5b
adamgdaniel <adamgdaniel@gmail.com>
2025-12-01 13:40:10 +0100

    cambios test

--- ingesta.py
+++ ingesta.py
@@ -4,6 +4,9 @@ import logging
 import psycopg
 import requests
 from urllib.parse import urlparse, urlunparse
+#from dotenv import load_dotenv
+
+#load_dotenv()
 
 # =======================================================
 # VARIABLES GLOBALES
@@ -32,45 +35,45 @@ logging.basicConfig(
 # ====================================
 # CREAR BASE DE DATOS
 # ====================================
-# def create_database():
-#     conn = None
-#     cur = None
-#     for _ in range(10):  # intenta 10 veces
-#         try:
-#             logging.info("Verificando base '%s'...", DB_NAME)
-#             conn = psycopg.connect(
-#                 host=DB_HOST,
-#                 port=DB_PORT,
-#                 user=DB_USER,
-#                 password=DB_PASS,
-#                 dbname="postgres",  # se conecta a postgres para crear la BD
-#                 connect_timeout=5
-#             )
-#             conn.autocommit = True
-#             cur = conn.cursor()
+def create_database():
+    conn = None
+    cur = None
+    for _ in range(10):  # intenta 10 veces
+        try:
+            logging.info("Verificando base '%s'...", DB_NAME)
+            conn = psycopg.connect(
+                host=DB_HOST,
+                port=DB_PORT,
+                user=DB_USER,
+                password=DB_PASS,
+                dbname="postgres",  # se conecta a postgres para crear la BD
+                connect_timeout=5
+            )
+            conn.autocommit = True
+            cur = conn.cursor()
 
-#             cur.execute(f"SELECT 1 FROM pg_database WHERE datname = '{DB_NAME}';")
-#             exists = cur.fetchone()
+            cur.execute(f"SELECT 1 FROM pg_database WHERE datname = '{DB_NAME}';")
+            exists = cur.fetchone()
             
-#             if not exists:
-#                 logging.info("Base no existe. Creándola...")
-#                 cur.execute(f"CREATE DATABASE {DB_NAME};")
-#                 logging.info(f"Base de datos '{DB_NAME}' creada correctamente.")
-#             else:
-#                 logging.info("La base ya existe.")
-#             cur.close()
-#             conn.close()
-#             return
+            if not exists:
+                logging.info("Base no existe. Creándola...")
+                cur.execute(f"CREATE DATABASE {DB_NAME};")
+                logging.info(f"Base de datos '{DB_NAME}' creada correctamente.")
+            else:
+                logging.info("La base ya existe.")
+            cur.close()
+            conn.close()
+            return
             
-#         except Exception as e:
-#             logging.error(f"Error creando database: {e}")
-#             if cur:
-#                 cur.close()
-#             if conn:
-#                 conn.close()
-#             time.sleep(3)
-#     logging.error("No se pudo crear/verificar la base de datos después de 10 intentos.")
-#     exit(1)
+        except Exception as e:
+            logging.error(f"Error creando database: {e}")
+            if cur:
+                cur.close()
+            if conn:
+                conn.close()
+            time.sleep(3)
+    logging.error("No se pudo crear/verificar la base de datos después de 10 intentos.")
+    exit(1)
 # ====================================
 # FUNCION ESPERAR CONEXIÓN
 # ====================================
@@ -134,10 +137,10 @@ def create_table():
                     );
                 """)
                 # Crear índice único para evitar duplicados
-                cur.execute("""
-                    CREATE UNIQUE INDEX IF NOT EXISTS idx_estaciones_objectid_fecha 
-                    ON estaciones(objectid, fecha_carg);
-                """)
+                # cur.execute("""
+                #     CREATE UNIQUE INDEX IF NOT EXISTS idx_estaciones_objectid_fecha 
+                #     ON estaciones(objectid, fecha_carg);
+                # """)
                 logging.info("Tabla e índice único creados correctamente.")
     except Exception as e:
         logging.error(f"Error guardando datos: {e}")
